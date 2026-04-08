0a40c67ce7409a6b438b087bb03b736e0a950f1e
Hubert Kuch <kuchhubert@gmail.com>
2026-04-08 14:20:00 +0200

    feat(main): initialize mcp server and register tools

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"praca-db-tools-mcp/tools"
+	"praca-db-tools-mcp/utils"
+
+	"github.com/mark3labs/mcp-go/server"
+)
+
+func main() {
+	_, err := utils.EnsureCacheDir()
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := server.NewMCPServer("db-tools-server", "1.0.0")
+
+	t1, h1 := tools.CreateRefreshDDLSchemaTool()
+	s.AddTool(t1, h1)
+
+	t2, h2 := tools.CreateExecuteReadonlyStatementTool()
+	s.AddTool(t2, h2)
+
+	t3, h3 := tools.CreateListDatabasesTool()
+	s.AddTool(t3, h3)
+
+	if err := server.ServeStdio(s); err != nil {
+		panic(err)
+	}
+}
